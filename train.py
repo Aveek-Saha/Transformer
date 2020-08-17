836d4cb4b11d4de3b72ac8f58faea1107dacf3f9
Aveek Saha <aveek.s98@gmail.com>
2020-08-18 01:10:00 +0530

    Create training pipline

---
+++ train.py
@@ -0,0 +1,198 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en', with_info=True,
+                               as_supervised=True)
+train_examples, val_examples = examples['train'], examples['validation']
+
+
+tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (en.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+    (pt.numpy() for pt, en in train_examples), target_vocab_size=2**13)
+
+
+sample_string = 'Transformer is awesome.'
+
+tokenized_string = tokenizer_en.encode(sample_string)
+print('Tokenized string is {}'.format(tokenized_string))
+
+original_string = tokenizer_en.decode(tokenized_string)
+print('The original string: {}'.format(original_string))
+
+assert original_string == sample_string
+
+for ts in tokenized_string:
+  print('{} ----> {}'.format(ts, tokenizer_en.decode([ts])))
+
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+
+def encode(lang1, lang2):
+  lang1 = [tokenizer_pt.vocab_size] + tokenizer_pt.encode(
+      lang1.numpy()) + [tokenizer_pt.vocab_size+1]
+
+  lang2 = [tokenizer_en.vocab_size] + tokenizer_en.encode(
+      lang2.numpy()) + [tokenizer_en.vocab_size+1]
+
+  return lang1, lang2
+
+
+def tf_encode(pt, en):
+  result_pt, result_en = tf.py_function(encode, [pt, en], [tf.int64, tf.int64])
+  result_pt.set_shape([None])
+  result_en.set_shape([None])
+
+  return result_pt, result_en
+
+
+MAX_LENGTH = 40
+
+
+def filter_max_length(x, y, max_length=MAX_LENGTH):
+  return tf.logical_and(tf.size(x) <= max_length,
+                        tf.size(y) <= max_length)
+
+
+train_dataset = train_examples.map(tf_encode)
+train_dataset = train_dataset.filter(filter_max_length)
+# cache the dataset to memory to get a speedup while reading from it.
+train_dataset = train_dataset.cache()
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE)
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+val_dataset = val_examples.map(tf_encode)
+val_dataset = val_dataset.filter(filter_max_length).padded_batch(BATCH_SIZE)
+
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+
+input_vocab_size = tokenizer_pt.vocab_size + 2
+target_vocab_size = tokenizer_en.vocab_size + 2
+dropout_rate = 0.1
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+
+  return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
+
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+    name='train_accuracy')
+
+transformer = Transformer(num_layers, d_model, num_heads, d_ff,
+                          input_vocab_size, target_vocab_size,
+                          pos_enc_input=input_vocab_size,
+                          pos_enc_target=target_vocab_size,
+                          rate=dropout_rate)
+
+checkpoint_path = "./checkpoints/train"
+
+ckpt = tf.train.Checkpoint(transformer=transformer,
+                           optimizer=optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+  print('Latest checkpoint restored!!')
+
+EPOCHS = 20
+
+train_step_signature = [
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
+
+
+@tf.function(input_signature=train_step_signature)
+def train_step(inp, tar):
+  tar_inp = tar[:, :-1]
+  tar_real = tar[:, 1:]
+
+  enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+      inp, tar_inp)
+
+  with tf.GradientTape() as tape:
+    predictions, _ = transformer(inp, tar_inp,
+                                 True,
+                                 enc_padding_mask,
+                                 combined_mask,
+                                 dec_padding_mask)
+    loss = loss_function(tar_real, predictions)
+
+  gradients = tape.gradient(loss, transformer.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(tar_real, predictions)
+
+
+for epoch in range(EPOCHS):
+  start = time.time()
+
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+
+  # inp -> portuguese, tar -> english
+  for (batch, (inp, tar)) in enumerate(train_dataset):
+    train_step(inp, tar)
+
+    if batch % 50 == 0:
+      print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+          epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+
+  if (epoch + 1) % 5 == 0:
+    ckpt_save_path = ckpt_manager.save()
+    print('Saving checkpoint for epoch {} at {}'.format(epoch+1,
+                                                        ckpt_save_path))
+
+  print('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                                      train_loss.result(),
+                                                      train_accuracy.result()))
+
+  print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+
+
